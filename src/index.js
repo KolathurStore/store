be6e28e5ba1a172bcfd38b0655a40bdcbe578f3d
Kolathur <sindhapari28@gmail.com>
2024-04-30 10:16:45 +0530

    recent

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+ReactDOM.render( <React.StrictMode> <BrowserRouter>
+ <App /> </BrowserRouter> </React.StrictMode>, document.getElementById('root'), )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
